e95b20a0572ca7c438471e217b6279809b2a6ab6
augustofrrr <114700112+augustofrrr@users.noreply.github.com>
2026-04-16 11:35:56 -0300

    Delete modal.js

--- modal.js
+++
@@ -1,6 +0,0 @@
-// ================================================================
-// MODAL
-// ================================================================
-function openModal(html){document.getElementById('mc').innerHTML=`<div class="modal-bg" onclick="bgClose(event)"><div class="modal">${html}</div></div>`;}
-function closeModal(){document.getElementById('mc').innerHTML='';}
-function bgClose(e){if(e.target===e.currentTarget)closeModal();}
